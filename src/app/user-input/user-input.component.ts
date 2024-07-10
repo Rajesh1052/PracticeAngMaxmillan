a66d37a0a896fe9cf596327dac24be15a18e55b2
Rjsh <rajesh.kumar1052@gmail.com>
2024-07-10 13:04:03 +0530

    Using Services And Signals In Services

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,7 +1,7 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
 
-import type { InvestmentInput } from './investment-input.model';
 @Component({
   selector: 'app-user-input',
   standalone: true,
@@ -10,14 +10,14 @@ import type { InvestmentInput } from './investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredDuration = signal('10');
   enteredExpectedReturn = signal('5');
 
+  constructor(private investmentService: InvestmentService) {}
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       duration: +this.enteredDuration(),
       expectedReturn: +this.enteredExpectedReturn(),
